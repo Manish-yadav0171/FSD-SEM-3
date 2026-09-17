410c1e851bd32de40b68172017c7c3f1f45f08d5
MANISH YADAV <maniishhh.21@gmail.com>
2026-09-17 21:07:03 +0530

    JAVASCRIPT

--- NODE/Streams/app.js
+++ NODE/Streams/app.js
@@ -1,22 +1,30 @@
 import fs from "fs";
-//Readable Streams
-const readStream=fs.createReadStream("input.txt",{encoding:"utf-8"});
-readStream.on("data",(chunk)=>{
+
+const readStream = fs.createReadStream("input.txt", {
+    encoding: "utf-8"
+});
+
+const writeStream = fs.createWriteStream("output.txt");
+
+readStream.on("data", (chunk) => {
     console.log("Data Received");
-    console.log("Data:",chunk);
-})
-readStream.on("end",()=>{
+    console.log("Data:", chunk);
+});
+
+readStream.on("end", () => {
     console.log("END");
-})
-readStream.on("error",(err)=>{
-    console.log("Error:",err.message);
-})
-const writeStream=fs.createWriteStream("output.txt");
-writeStream.write("Hello World\n");
-writeStream.on("finish",()=>{
+});
+
+readStream.on("error", (err) => {
+    console.log("Read Error:", err.message);
+});
+
+writeStream.on("finish", () => {
     console.log("Data Written");
-})
-writeStream.on("error",(err)=>{
-    console.log("Error:",err.message);
-})
+});
+
+writeStream.on("error", (err) => {
+    console.log("Write Error:", err.message);
+});
+
 readStream.pipe(writeStream);
